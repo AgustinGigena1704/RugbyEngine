d136abf89439395e67ba9e58a0a3dec0d7c3f465
Agustin Gigena <agustingigena1704@gmail.com>
2026-04-04 15:30:19 -0300

    fix

--- RugbyEngine.Shared/Menus/RouteRoleDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace RugbyEngine.Shared.Menus
-{
-    public class RouteRoleDTO
-    {
-        public required string Route { get; set; }
-        public required string Role { get; set; }
-    }
-}
